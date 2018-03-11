57af774d4e68750f3033184faf3699f9c9923b67
Madhavi Latha Ambati <madhavilatha.ambati@valtech.co.in>
2018-03-11 13:54:24 +0530

    service and dependency injection and its scope definations

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit, OnDestroy } from '@angular/core';
+import { Injectable, } from '@angular/core';
 
 // Business logic communication with webservice
 // data sharing among components
@@ -10,15 +10,16 @@ import { Injectable, OnInit, OnDestroy } from '@angular/core';
 
 
 @Injectable()
-export class DataService implements OnInit, OnDestroy{
+export class DataService {
+ 
+  // OnInit, OnDestroy -- should be used only for the components, directives and pipes but not on services.
 
-
-  ngOnDestroy(): void {
-   console.log("data service on Destroy")
-  }
-  ngOnInit(): void {
-    console.log("data service on Init")
-  }
+  // ngOnDestroy(): void {
+  //  console.log("data service on Destroy")
+  // }
+  // ngOnInit(): void {
+  //   console.log("data service on Init")
+  // }
   constructor() { 
     console.log("DataService created");
   }
