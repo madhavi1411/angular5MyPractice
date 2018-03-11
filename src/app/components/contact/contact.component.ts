4370ec1d15ead3238e35efc9cd1c914b244dd06c
Madhavi Latha Ambati <madhavilatha.ambati@valtech.co.in>
2018-03-11 09:26:38 +0530

    dom element ref and intellisense feature usages

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import * as $ from 'jquery'; //jquery can go beyond the component boundary
+
+interface Address {
+  city: string;
+  pincode: number;
+}
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +14,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  // <input #name />
+  @ViewChild("name")
+  nameInput: ElementRef;
+
+  @ViewChild("para1")
+  para1 : ElementRef;
+
+  // @ViewChild("rootDiv")
+  // rootDiv : ElementRef;
+
+address: Address; //undefined
+
+
+  //dependency injection
+  //option2 , angular shall inject top element no need to specify #rootDiv on top of the div element
+  constructor(private topDiv: ElementRef) { }
 
   ngOnInit() {
+    //nativeElement is the DOM object
+    this.nameInput.nativeElement.focus(); 
+    this.nameInput.nativeElement.value = 'From TS'; 
+
+    //BAD
+    // $("p").text("Hi JQuery");
+
+    //JQuery Scoped search
+    // $(this.rootDiv.nativeElement).find("p").text("Hi Scoped JQuery");
+    $(this.topDiv.nativeElement).find("p").text("Hi Scoped JQuery");
+
+    setTimeout(() => {
+      this.address = {city: 'BLR', pincode: 560078};
+    }, 3000);
+
+    //Intellisense for jquery
+    
+
   }
 
 }
