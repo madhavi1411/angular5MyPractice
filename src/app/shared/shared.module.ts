58eb0fd0a53923027e5ab21e2cc44e6273d7678b
Madhavi Latha Ambati <madhavilatha.ambati@valtech.co.in>
2018-03-11 09:46:06 +0530

    added some shared components directives and pipes

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddressComponent } from './components/address/address.component';
+import { LikeComponent } from './components/like/like.component';
+import { HighlightDirective } from './directives/highlight.directive';
+import { PowerPipe } from './pipes/power.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AddressComponent, LikeComponent, HighlightDirective, PowerPipe, FilterPipe]
+})
+export class SharedModule { }
