e60c5437aff3ce0f7ef292483829378b719d72fb
Madhavi Latha Ambati <madhavilatha.ambati@valtech.co.in>
2018-03-12 12:00:54 +0530

    product edit and create

--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ProductService } from '../services/product.service';
+
+// GET /api/products/34345
+// Edit existing data (id exist) or create new one (id is not present)
+
+// the following import helps to read the data from the url parameter
+import {ActivatedRoute, //read url params
+      Router // helps to navigate between pages
+  } from '@angular/router';
+import { Product } from '../models/product';
+import { Brand } from '../models/brand';
+import { Subscription } from 'rxjs/Subscription';
+
 
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css']
 })
-export class ProductEditComponent implements OnInit {
+export class ProductEditComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  product: Product = new Product(); //create 
+  brands: Brand[] = [];
+  
+  // to stop the processing request when component is not in scope.
+  subscription: Subscription;
+
+  constructor(private route: ActivatedRoute, private router: Router, private productService: ProductService) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    console.log (" ID is : ", id );
+
+    // GET product with id
+    this.subscription = this.productService.getProduct(id).subscribe(product => {
+      this.product = product;
+    });
+
+    // GET list of all Brands
+    this.productService.getBrands().subscribe(brands => {
+      this.brands = brands;
+    });
+    
   }
 
+  gotoList() {
+    this.router.navigateByUrl('/products');  // goes to product list 
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  saveProduct() {
+    console.log("Saved Product: ", this.product);
+
+    this.productService.saveProduct(this.product).subscribe(savedProduct => {
+
+      //option 1: continue working same form
+      this.product = savedProduct;
+
+      //option 2: goto to list page.
+      this.gotoList();
+    });   
+  }
 }
--- src/app/product/services/product.service.ts
+++ src/app/product/services/product.service.ts
@@ -4,12 +4,13 @@ import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Product } from '../models/product';
 import { environment } from '../../../environments/environment';
+import { Brand } from '../models/brand';
 
 
 console.log("ENV: " , environment)
 
 @Injectable()
-export class ProductService {
+export class ProductService { 
 
   // Dependency injection (DI)
   constructor(private http: HttpClient) { 
@@ -20,11 +21,55 @@ export class ProductService {
     // let productApi : string = 'http://localhost:7070/api/products';
     // let delayedProductApi: string = "http://localhost:7070/delayed/api/products";
 
-
-    let productApi : string = `${environment.apiEndPoint}/api/products`;
-    let delayedProductApi: string = `${environment.apiEndPoint}/delayed/api/products`;
+    const productApi : string = `${environment.apiEndPoint}/api/products`;
+    const delayedProductApi: string = `${environment.apiEndPoint}/delayed/api/products`;
+   
     
     return this.http.get<Product[]>(productApi);
   }
+
+  getBrands(): Observable<Brand[]> {
+    // let productApi : string = 'http://localhost:7070/api/products';
+    // let delayedProductApi: string = "http://localhost:7070/delayed/api/products";
+
+    const brandsapi : string = `${environment.apiEndPoint}/api/brands`;
+   
+    return this.http.get<Brand[]>(brandsapi);
+  }
+
+  // GET /api/products/12345
+  getProduct(id): Observable<Product> {
+    return this.http.get<Product>(`${environment.apiEndPoint}/api/products/${id}`);
+  }
+
+  //update
+  //PUT /api/products/123
+  // {json data}
+
+  // server receives, commmit data to db
+  // server read data from db, sends back to client
+  private updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(`${environment.apiEndPoint}/api/products/${product.id}`, product);
+  }
+  
+
+  //Create no ID
+  //POST /api/products
+  // {json data}
+
+  // server receives, commit data to db, server generates id
+  // server read data from db, sends back to client, with id.
+  private createProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(`${environment.apiEndPoint}/api/products`, product);
+  }
+
+
+  saveProduct(product: Product): Observable<Product> {
+    if (product.id) {
+      return this.updateProduct(product);
+    }
+ 
+    return this.createProduct(product);
+ }
 
 }
